72822073f401d1cd1e9f323f20c5110754e0b842
Eak Netpanya <eak.netpanya@gmail.com>
2025-04-18 07:20:36 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,8 @@
-import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import Link from "next/link";
-
-import "./globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Sidebar } from "./components/Sidebar";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "one.ilmsg.in.th",
-  description: "weblog ilmsg.in.th",
+  title: 'Next.js on GitHub Pages',
+  description: 'Deploy your static Next.js site to GitHub Pages.',
 };
 
 export default function RootLayout({
@@ -28,23 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {/* <nav className="flex space-x-4">
-          <Link href="/">About</Link>&nbsp;|&nbsp;
-          <Link href="/posts">Posts</Link>
-        </nav> */}
-
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-3" style={{ 'fontSize': 12.5 }}>
-              <Sidebar />
-            </div>
-            <div className="col-sm-9">
-              <div className="container-fluid">{children}</div>;
-            </div>
-          </div>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: "docs",
-  output: 'export'
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
