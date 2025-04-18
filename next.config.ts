8fdbc94d486ca063e488625eff5bafe085d7bb65
Eak Netpanya <eak.netpanya@gmail.com>
2025-04-18 21:34:57 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   distDir: "docs",
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  // basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
