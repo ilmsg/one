02866df6be4551aa9fc8b3a56c38acca53195ab2
Eak Netpanya <eak.netpanya@gmail.com>
2025-04-18 12:31:25 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next';
 
+import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Sidebar } from "./components/Sidebar";
+
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: 'one.ilmsg.in.th',
+  description: 'weblog ilmsg.in.th',
 };
 
 export default function RootLayout({
@@ -12,7 +16,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-3" style={{ 'fontSize': 12.5 }}>
+              <Sidebar />
+            </div>
+            <div className="col-sm-9">
+              <div className="container-fluid">{children}</div>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ app/posts/page.tsx
@@ -0,0 +1,16 @@
+import { getAllPosts, getPostSlugs, PostType } from "@/lib/get-posts"
+import Link from "next/link";
+
+export default async function Posts() {
+    // const slugs = await getPostSlugs()
+    // console.log('slugs:', slugs);
+
+    // const posts = await getAllPosts()
+    // console.log('posts:', posts)
+
+    return (
+        <>
+            <h2>Post List</h2>
+        </>
+    )
+}
